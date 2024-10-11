df4ac225b653838db70f7d0ed3a793ea6f9f45f4
aubreyjcook <aubreyjcook@gmail.com>
2024-10-11 14:02:36 -0500

    add base template-redirect file

---
+++ 404s/quackwatch/template-redirect/php/template-redirect.php
@@ -0,0 +1,7 @@
+add_action( 'template_redirect', 'custom_handle_404_redirect' );
+
+function custom_handle_404_redirect() {
+    if ( is_404() ) {
+        // Your custom redirect logic here
+    }
+}
